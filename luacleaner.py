a045358ad715e4a5f6ce714a5960b8d2ce755793
Dragonarab <dragonarab@gmail.com>
2023-04-21 17:12:57 +0300

    v1.1
    Update script to handle nested multi-line comments

--- luacleaner.py
+++ luacleaner.py
@@ -2,12 +2,16 @@
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License.
 
+import re
+
 def remove_comments_from_lua(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as f:
         content = f.read()
 
     cleaned_content = []
     inside_multiline_comment = False
+    comment_level = -1
+
     for line in content.splitlines(True):
         cleaned_line = line
 
@@ -17,14 +21,18 @@ def remove_comments_from_lua(input_file, output_file):
                 cleaned_line = line.split('--', 1)[0] + '\n'
 
             # Removing multi-line comments
-            if '--[[' in line:
-                cleaned_line = line.split('--[[', 1)[0]
+            multiline_comment_start = re.search(r'--\[(=*)\[', line)
+            if multiline_comment_start:
+                cleaned_line = line.split(multiline_comment_start.group(0), 1)[0]
                 inside_multiline_comment = True
+                comment_level = len(multiline_comment_start.group(1))
 
         if inside_multiline_comment:
-            if ']]' in line:
-                cleaned_line = cleaned_line.split(']]', 1)[1]
+            multiline_comment_end = re.search(r']=(=*)]', line)
+            if multiline_comment_end and len(multiline_comment_end.group(1)) == comment_level:
+                cleaned_line = cleaned_line.split(multiline_comment_end.group(0), 1)[1]
                 inside_multiline_comment = False
+                comment_level = -1
 
         cleaned_content.append(cleaned_line)
 
